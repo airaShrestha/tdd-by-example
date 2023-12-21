b4e5f84127ea58cc91f902aff35f44bced735a3a
airaShrestha <ashre009@ucr.edu>
2023-12-21 12:51:34 -0600

    addition,finally

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -30,10 +30,18 @@ public class MoneyTest {
         assertEquals(Money.franc(15), five.times(3));
     }
 
-    
+
     @Test
     void testCurrency(){
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
+    }
+
+    void testSimpleAddition(){
+        Money five= Money.dollar(5);
+        Expression sum = five.plus(five);
+        Bank bank = new Bank ();
+        Money reduced = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(10), reduced);
     }
 }
